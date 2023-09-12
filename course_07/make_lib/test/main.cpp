04fb64670adbd838791745b23c4e34e31ac5482d
HuPengsheet <1554414201@qq.com>
2023-09-12 17:55:34 +0800

    add course_07

---
+++ course_07/make_lib/test/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include <fstream>
+
+#include<my_math.hpp>
+
+
+using namespace std;
+int main(){
+    cout<<"3+2="<<add(2,3)<<endl;
+    cout<<"3-2="<<sub(3,2)<<endl;
+}
---
+++ course_07/use_lib/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include"my_math.hpp"
+
+using namespace std;
+
+
+int main(){
+
+
+    cout<<"10+1="<<add(10,1)<<endl;
+    return 0;
+}
